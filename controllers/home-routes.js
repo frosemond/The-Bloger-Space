852ca73a51ca704e124cc708317773015d5a7546
doldol123 <toruig59@gmail.com>
2022-11-05 20:39:30 -0700

    create GET a single post by ID function in home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -28,3 +28,30 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// get one post by ID
+router.get('/post/:id', async (req, res) => {
+    if(!req.session.loggedIn) {
+        res.redirect('/login');
+    } else {
+        try {
+            const dbPostData = await Post.findByPk(req.params.id, {
+                include: [
+                    {
+                        model: Comment,
+                        attributes: [
+                            'id',
+                            'comment_detail',
+                        ],
+                    },
+                ],
+            });
+            const post = dbPostData.get({ plain: true });
+            res.render('post', {post, loggedIn: req.session.loggedIn });
+        } catch(err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    }
+});
+
