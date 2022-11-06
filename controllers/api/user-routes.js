a173d32a8c19570d8146dfb0354db1b7d89390d7
doldol123 <toruig59@gmail.com>
2022-11-05 20:56:03 -0700

    create POST for user login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -19,3 +19,31 @@ router.post('/', async(req,res) => {
     }
 });
 
+// user login
+router.post('/login', async(req, res) => {
+    try {
+        const dbUserData = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        });
+        
+        if(!dbUserData) {
+            res
+            .status(400)
+            .json({message: 'Incorrect username or password. Please try again!'});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res
+                .status(200)
+                .json({user: dbUserData, message: 'You are now logged in!'});
+        });
+    } catch(err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+});
