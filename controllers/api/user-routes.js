2d66f1c2868d286b72dbd2b5bb6d2b9fc29fd46b
doldol123 <toruig59@gmail.com>
2022-11-05 20:58:04 -0700

    create POST for user logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require ('express').Router();
+const { resourceUsage } = require('process');
 const { User } = require('../../models');
 
 // new user signup
@@ -47,3 +48,16 @@ router.post('/login', async(req, res) => {
         res.status(500).json(err);
     }
 });
+
+// user logout
+router.post('/logout', (req,res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
