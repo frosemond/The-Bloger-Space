acb9c9788820a36092b83d745620b451b259a19c
doldol123 <toruig59@gmail.com>
2022-11-06 12:54:27 -0800

    create add-post function

---
+++ public/js/add-post.js
@@ -0,0 +1,25 @@
+const addPostFormHandler = async (e) => {
+    e.preventDefault();
+
+    const title = $("#post-title").value;
+    const content = $("#post-content").value;
+
+    const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            content
+        }),
+        headers: {
+            'content-type': 'application/json' 
+        }
+    });
+
+    if(response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Request Failed!');
+    }
+ }
+
+ $("post-form").on("submit", addPostFormHandler);
