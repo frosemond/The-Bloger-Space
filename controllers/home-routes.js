9db604adddc71d393e71fb18dfc153ba6cad8009
doldol123 <toruig59@gmail.com>
2022-11-05 22:56:28 -0700

    fix typo

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -83,7 +83,7 @@ router.get('/login', (req, res) => {
     if(req.session.loggedIn) {
         res.redirect('/');
         return;
-    };
+    }
     res.render('login');
 });
 
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
-const sesssion = require('express-session');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(sesssion.Store);
-const handlebars = require('handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const handlebars = require('handlebars');
+const hbs = exphbs.create;
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -21,9 +22,9 @@ const sess = {
     }),
 };
 
-app.use(sesssion(sess));
+app.use(session(sess));
 app.set('views', path.join(__dirname))
-// app.engine('handlebars', hbs.engine);
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
