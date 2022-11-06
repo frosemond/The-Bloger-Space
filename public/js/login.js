18a606b7914854699c123f6fe5e7f1aaee747669
doldol123 <toruig59@gmail.com>
2022-11-06 14:27:46 -0800

    move signup function out of login.js to signup.js

--- public/js/login.js
+++ public/js/login.js
@@ -19,28 +19,6 @@ const loginFormHandler = async (e) => {
     }
 };
 
-const signupFormHandler = async (e) => {
-    e.preventDefault();
-
-    const userName = $("#username-signup").value.trim();
-    // const email = $("#email-signup").value.trim();
-    const password = $("#password-signup").value.trim();
-
-    if (userName && password) {
-        const response = await fetch ('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({userName, password}),
-            headers: {'content-type' : 'application/json'},
-        });
-
-        if(response.ok) {
-            document.location.replace('/');
-        } else {
-            alert('Fail to sign up!');
-        }
-    }
-};
 
 $(".login-form").on("submit", loginFormHandler);
 
-$(".signup-form").on("submit", signupFormHandler);
---
+++ public/js/signup.js
@@ -0,0 +1,23 @@
+
+const signupFormHandler = async (e) => {
+    e.preventDefault();
+
+    const userName = $("#username-signup").value.trim();
+    const password = $("#password-signup").value.trim();
+
+    if (userName && password) {
+        const response = await fetch ('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({userName, password}),
+            headers: {'content-type' : 'application/json'},
+        });
+
+        if(response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Fail to sign up!');
+        }
+    }
+};
+
+$(".signup-form").on("submit", signupFormHandler);
