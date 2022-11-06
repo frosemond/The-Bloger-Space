97ad8fbdf6855777a0e16ed41f4f9ee6ff7dd6c4
doldol123 <toruig59@gmail.com>
2022-11-05 18:52:47 -0700

    add signupFormHandler function

--- public/js/login.js
+++ public/js/login.js
@@ -11,11 +11,33 @@ const loginFormHandler = async (e) => {
             headers: {'content-type' : 'application/json'},
         });
 
-    if (response.ok) {
-        document.location.replace('/');
-    } else {
-        alert('Failed to log in!');
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log in!');
+        }
     }
+};
+
+const signupFormHandler = async (e) => {
+    e.preventDefault();
+
+    const userName = $("#username-signup").value.trim();
+    // const email = $("#email-signup").value.trim();
+    const password = $("#password-signup").value.trim();
+
+    if (userName && password) {
+        const response = await fetch ('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({userName, password}),
+            headers: {'content-type' : 'application/json'},
+        });
+
+        if(response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Fail to sign up!');
+        }
     }
 };
 
