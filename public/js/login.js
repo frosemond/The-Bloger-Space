84db7e466bcf8764e44f0a306901e7914c3cbfbc
doldol123 <toruig59@gmail.com>
2022-11-05 18:46:53 -0700

    add loginformhandler function

---
+++ public/js/login.js
@@ -0,0 +1,21 @@
+const loginFormHandler = async (e) => {
+    e.preventDefault();
+
+    const userName = $("#user-login").value.trim();
+    const password = $("#password-login").value.trim();
+
+    if (userName && password) {
+        const response = await fetch ('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({userName, password}),
+            headers: {'content-type' : 'application/json'},
+        });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to log in!');
+    }
+    }
+};
+
