43cad82bdc5535440337a869bd3a60cb6532a23a
doldol123 <toruig59@gmail.com>
2022-11-06 12:34:26 -0800

    change path for auth

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models');
 const sequelize = require('../config/connection');
-const withAuth = require('../utils/helper');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     Post.findAll({
--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models');
-const withAuth = require('../utils/helper');
+const withAuth = require('../utils/auth');
 const sequelize = require('../config/connection');
 
 router.get('/', withAuth, (req, res) => {
