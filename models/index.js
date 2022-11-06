98f5534614107bc6efac45b0deb8aaa55c6c1cc9
doldol123 <toruig59@gmail.com>
2022-11-06 11:45:20 -0800

    complete model-index.js with user relationship

--- models/index.js
+++ models/index.js
@@ -2,12 +2,34 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 const User = require('./User');
 
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
 Post.hasMany(Comment, {
     foreignKey: 'post_id',
+    onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(Post, {
     foreignKey: 'post_id',
+    onDelete: 'CASCADE'
 });
 
-module.exports = { Post, Comment};
+
+module.exports = { Post, Comment, User};
